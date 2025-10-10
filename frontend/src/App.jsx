5b6bead06d4cee0d27c66048d34ce20920ba8398
Alexis Lasselin <114481578+AlexisLasselin@users.noreply.github.com>
2025-10-10 10:18:44 +0200

    connect front and back

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,14 +50,14 @@ function App() {
     setTimeout(() => setStars([]), 1500);
 
     setLoading(true);
-    const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-    
+
     try {
-      const res = await fetch(`${API_URL}/parse-json`, {
+      const res = await fetch("http://interpreter:5000/parse-json", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ code }),
       });
+
       const data = await res.json();
       setOutput(data.output || []);
 
