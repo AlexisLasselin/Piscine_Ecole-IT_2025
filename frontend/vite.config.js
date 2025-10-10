cce937676ac1457a05373335e10ea4cb1deead4a
Alexis Lasselin <114481578+AlexisLasselin@users.noreply.github.com>
2025-10-10 14:38:58 +0200

    front image improvement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({ algorithm: "brotliCompress" }), // Génère fichiers .br
+  ],
   server: {
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+  build: {
+    sourcemap: false,
+    minify: "terser", // Minification agressive
+  },
+});
